6d657b8dd7b5852b99f851bceec20df9f5810ad1
Arava <96862533+aravaweb@users.noreply.github.com>
2023-05-09 23:18:53 +0400

    wed tt

--- js/bell/classdisplay.js
+++ js/bell/classdisplay.js
@@ -6,19 +6,15 @@
 
  // Set the text to display based on the day and time
  if (day == 2 && hour >= 7 && minute >= 15) {
-   document.getElementById("displayText").innerHTML = "10B";
- } else if (day == 2 && hour >= 7 && minute >= 55) {
    document.getElementById("displayText").innerHTML = "10A";
+ } else if (day == 2 && hour >= 7 && minute >= 55) {
+   document.getElementById("displayText").innerHTML = "10B";
  } else if (day == 2 && hour >= 8 && minute >= 35) {
    document.getElementById("displayText").innerHTML = "Break";
- } else if (day == 2 && hour >= 9 && minute >= 25) {
+ } else if (day == 2 && hour >= 11 && minute >= 10) {
    document.getElementById("displayText").innerHTML = "10A";
- } else if (day == 2 && hour >= 10) {
-   document.getElementById("displayText").innerHTML = "Break";
- } else if (day == 2 && hour >= 11 && minute >= 45) {
-   document.getElementById("displayText").innerHTML = "10B";
  } else if (day == 2 && hour >= 12 && minute >= 20) {
-   document.getElementById("displayText").innerHTML = "10A";
+   document.getElementById("displayText").innerHTML = "10B";
  } else {
    document.getElementById("displayText").innerHTML = "No Class";
  }
